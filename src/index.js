cd34e4473c4d0c48b698c32f20174672fc29525e
rafid <rafidadib123@gmail.com>
2024-12-17 11:37:03 +0600

    utils code iscussions

--- src/index.js
+++ src/index.js
@@ -8,10 +8,22 @@ dotenv.config({
 // import { DB_NAME } from "./constants";
 // import express from "express";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT|| 8000, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+
+}
+)
+.catch((err)=>{
+  console.log("there is error while connecting mongodb",err);
+})
+
 
 /*
 const app = express()
